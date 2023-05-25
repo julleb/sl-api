3fb4330ab272b47b64e7979476414a5546cd3fb1
julleb <you@example.com>
2023-05-25 20:31:42 +0200

    Printar enligt assignment

--- src/main/java/se/slapi/service/AssignmentTask.java
+++ src/main/java/se/slapi/service/AssignmentTask.java
@@ -26,6 +26,7 @@ public class AssignmentTask {
 
     public List<Busline> task(BuslineService buslineService) throws ServiceException {
         var buslines = buslineService.getBuslines();
+        if(buslines.isEmpty()) return buslines;
         var sortedBuslines = buslines.stream().sorted(Comparator.comparing(b -> getLongestTrafficRouteForBusline(b).stopPoints().size())).toList();
         List<Busline> reversedBuslines = new ArrayList<>();
         for(int i = sortedBuslines.size() - 1; i >= 0; i--) {
@@ -33,11 +34,20 @@ public class AssignmentTask {
         }
 
         reversedBuslines = reversedBuslines.stream().limit(10).toList();
-        System.out.println("LISTING TOP 10 BUS LINES");
+        System.out.println("LISTING TOP 10 BUS LINES WITH MOST BUS STOPS");
         for(var busline : reversedBuslines) {
             TrafficRoute trafficRoute = getLongestTrafficRouteForBusline(busline);
-            System.out.println("BusLine="+busline.id() + " AmountOfStops="+ trafficRoute.stopPoints().size() + " StopNames="+trafficRoute.stopPoints());
+            System.out.println("BusLine="+busline.id() + " AmountOfStops="+ trafficRoute.stopPoints().size());
         }
+
+        var buslineWithMostStops = reversedBuslines.get(0);
+        TrafficRoute trafficRoute = getLongestTrafficRouteForBusline(buslineWithMostStops);
+        System.out.println("LISTING STOP NAMES FOR Busline=" + buslineWithMostStops.id());
+        List<String> stopNames = new ArrayList<>();
+        for(var stopPoint : trafficRoute.stopPoints()) {
+            stopNames.add(stopPoint.name());
+        }
+        System.out.println(stopNames);
         return reversedBuslines;
     }
 }
