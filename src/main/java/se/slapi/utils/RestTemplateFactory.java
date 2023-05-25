4d170d36343b903371df572680c0a930bf92a575
julleb <you@example.com>
2023-05-25 18:22:35 +0200

    Timeout på RestTemplate

--- src/main/java/se/slapi/utils/RestTemplateFactory.java
+++ src/main/java/se/slapi/utils/RestTemplateFactory.java
@@ -2,14 +2,20 @@ package se.slapi.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateFactory {
 
+    private static int connectionTimeOutInMs = 10 * 1000;
+    private static int readTimeOutInMs = 10 * 1000;
+
     @Bean
     public RestTemplate restTemplate() {
-        //TODO set http timeout and so on
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(connectionTimeOutInMs);
+        factory.setReadTimeout(readTimeOutInMs);
+        return new RestTemplate(factory);
     }
 }
